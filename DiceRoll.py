596c7c8fcf68d40d2606c6f32dee8ea8213949cd
Sara <136837861+SaraBhatti@users.noreply.github.com>
2023-09-14 15:05:52 +0100

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -11,29 +11,29 @@ while x == "r":
  
 #Returns a print of an actual dice number
     if dice == 1:
-        print("[     ]")
-        print("[  0  ]")
-        print("[     ]")
+        print("|     |")
+        print("|  0  |")
+        print("|     |")
     if dice == 2:
-        print("[ 0   ]")
-        print("[     ]")
-        print("[   0 ]")
+        print("| 0   |")
+        print("|     |")
+        print("|   0 |")
     if dice == 3:
-        print("[    0]")
-        print("[  0  ]")
-        print("[0    ]")
+        print("|    0|")
+        print("|  0  |")
+        print("|0    |")
     if dice == 4:
-        print("[0   0]")
-        print("[     ]")
-        print("[0   0]")
+        print("|0   0]")
+        print("|     |")
+        print("|0   0|")
     if dice == 5:
-        print("[0   0]")
-        print("[  0  ]")
-        print("[0   0]")
+        print("|0   0|")
+        print("|  0  |")
+        print("|0   0|")
     if dice == 6:
-        print("[0   0]")
-        print("[0   0]")
-        print("[0   0]")
+        print("|0   0|")
+        print("|0   0|")
+        print("|0   0|")
 
  #Press r key to roll again
     x=input("press r again and n to exit:")
